2cb6a7294a083b41c863a75ac8a539eb1bb74d78
André Säll <andresall95@hotmail.com>
2023-07-07 22:45:33 +0200

    Update dist folder path. Fix cdk synth errors.

--- Infra/lib/pipeline-dependencies-stack.ts
+++ Infra/lib/pipeline-dependencies-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { ArnPrincipal, Effect, FederatedPrincipal, OpenIdConnectProvider, PolicyStatement, Role } from 'aws-cdk-lib/aws-iam';
+import { Effect, FederatedPrincipal, OpenIdConnectProvider, PolicyStatement, Role } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class PipelineDependenciesStack extends cdk.Stack {
@@ -13,18 +13,18 @@ export class PipelineDependenciesStack extends cdk.Stack {
     });
 
     const githubDeployRole = new Role(this, 'GitHubDeployRole', {
-      assumedBy: new ArnPrincipal(oidcProvider.openIdConnectProviderArn)
+      assumedBy: new FederatedPrincipal(oidcProvider.openIdConnectProviderArn)
     });
     githubDeployRole.addToPolicy(new PolicyStatement({
       effect: Effect.ALLOW,
-      principals: [ new FederatedPrincipal(oidcProvider.openIdConnectProviderArn)],
       actions: ['sts:AssumeRoleWithWebIdentity'],
       conditions: {
         'StringEquals': {
           'token.actions.githubusercontent.com:sub': 'repo:TankSlayer1337/circuits-frontend:*',
           'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com'
         }
-      }
+      },
+      resources: [ 'arn:aws:iam::*:role/cdk-*' ]
     }));
   }
 }
