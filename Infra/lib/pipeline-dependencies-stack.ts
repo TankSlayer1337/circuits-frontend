ab617015394e650bdcc82ace37b95d8f9a88e2a2
André Säll <andresall95@hotmail.com>
2023-07-08 10:26:01 +0200

    Fix branch condition.

--- Infra/lib/pipeline-dependencies-stack.ts
+++ Infra/lib/pipeline-dependencies-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { Effect, FederatedPrincipal, OpenIdConnectPrincipal, OpenIdConnectProvider, PolicyStatement, PrincipalWithConditions, Role } from 'aws-cdk-lib/aws-iam';
+import { OpenIdConnectPrincipal, OpenIdConnectProvider, Role } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class PipelineDependenciesStack extends cdk.Stack {
@@ -12,26 +12,13 @@ export class PipelineDependenciesStack extends cdk.Stack {
       clientIds: ['sts.amazonaws.com'],
     });
 
-    // const githubDeployRole = new Role(this, 'GitHubDeployRole', {
-    //   assumedBy: new FederatedPrincipal(oidcProvider.openIdConnectProviderArn)
-    // });
-    // githubDeployRole.addToPolicy(new PolicyStatement({
-    //   effect: Effect.ALLOW,
-    //   actions: ['sts:AssumeRoleWithWebIdentity'],
-    //   conditions: {
-    //     'StringEquals': {
-    //       'token.actions.githubusercontent.com:sub': 'repo:TankSlayer1337/circuits-frontend:*',
-    //       'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com'
-    //     }
-    //   },
-    //   resources: [ 'arn:aws:iam::*:role/cdk-*' ]
-    // }));
-
     const githubDeployRole = new Role(this, 'GitHubDeployRole', {
       assumedBy: new OpenIdConnectPrincipal(oidcProvider, {
         'StringEquals': {
-          'token.actions.githubusercontent.com:sub': 'repo:TankSlayer1337/circuits-frontend:*',
           'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com'
+        },
+        'StringLike': {
+          'token.actions.githubusercontent.com:sub': 'repo:TankSlayer1337/circuits-frontend:*'
         }
       })
     });
